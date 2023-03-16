42652bee1b566888b27d9f3dbe100f91da29d51f
Razvan Crainea <razvan@opensips.org>
2023-03-16 09:59:22 +0000

    02.registration-db: cleanup scenario

--- 02.registration-db/scripts/mysql-register.sh
+++ 02.registration-db/scripts/mysql-register.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-l=$(mysql opensips -Nse 'select * from location' | wc -l)
+l=$(mysql opensips -Nse 'select count(*) from location')
 if [ "$1" = "unregister" ]; then
 	[ "$l" == "0" ] && exit 0
 else
 	[ "$l" == "1" ] && exit 0
 fi
-echo "ERROR: length is $l"
+echo "ERROR: number of contacts is $l"
 exit 1
