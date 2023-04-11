993c6ed3aa1e1a9717ff6021a584b57ffa5d6edc
Vlad Patrascu <vladp@opensips.org>
2023-04-11 23:14:17 +0300

    b2b: adapt scenarios to opensips 3.4 bridging mechanism

--- run-all.sh
+++ run-all.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 # exclude tests that we know they fail for sure
-PARAMS="--exclude b2b/09.refer-unattended-uac-notify-fail
-	--exclude b2b/10.refer-unattended-uas-notify-fail
-	--exclude b2b/15.refer-unattended-uac-prov-notify-fail
-	--exclude b2b/16.refer-unattended-uas-prov-notify-fail
-	--exclude topology-hiding/02.th-no-dialog-username"
+PARAMS="--exclude topology-hiding/02.th-no-dialog-username"
 
 sipssert \
 	$PARAMS \
