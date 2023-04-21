2a69b8b3105d001966b6cf98719dcec406cf7a54
Razvan Crainea <razvan@opensips.org>
2023-04-21 18:24:38 +0300

    uac-auth: add initial tests

--- run-all.sh
+++ run-all.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
 # exclude tests that we know they fail for sure
-PARAMS=${PARAMS:---exclude topology-hiding/02.th-no-dialog-username}
+PARAMS=${PARAMS:---exclude topology-hiding/02.th-no-dialog-username \
+				 --exclude uac-auth/17.dialog-auth-reinvite-uas \
+				 --exclude uac-auth/20.dialog-auth-invite-reinvite-bye-uas \
+				 --exclude uac-auth/22.dialog-auth-reinvite-uac \
+				 --exclude uac-auth/23.dialog-auth-reinvite-bye-uac \
+				 --exclude uac-auth/24.dialog-auth-reinvite-uac-reinvite-uas \
+				 --exclude uac-auth/25.dialog-auth-reinvite-uas-reinvite-uac \
+				 --exclude uac-auth/26.dialog-auth-reinvite-uac-reinvite-uas-bye-uas \
+				 --exclude uac-auth/27.dialog-auth-reinvite-uac-reinvite-uas-bye-uac \
+		}
+
 SETS=${SETS:-registration \
 	auth \
 	record-route \
 	dialog \
 	topology-hiding \
-	b2b}
+	b2b \
+	uac-auth}
 
 sipssert \
 	$PARAMS \
