2390d2cf3a20b6e8a62537a07f35680339a0847e
Razvan Crainea <razvan@opensips.org>
2023-08-04 14:19:58 +0300

    do not exclude working uac-auth tests
    (cherry picked from commit 1313d03b6ecd1972f9d2facf69116c418fb40399)

--- run-all.sh
+++ run-all.sh
@@ -6,14 +6,6 @@ PARAMS=${PARAMS:---exclude b2b/09.refer-unattended-uac-notify-fail \
 				 --exclude b2b/15.refer-unattended-uac-prov-notify-fail \
 				 --exclude b2b/16.refer-unattended-uas-prov-notify-fail \
 				 --exclude topology-hiding/02.th-no-dialog-username \
-				 --exclude uac-auth/17.dialog-auth-reinvite-uas \
-				 --exclude uac-auth/20.dialog-auth-invite-reinvite-bye-uas \
-				 --exclude uac-auth/22.dialog-auth-reinvite-uac \
-				 --exclude uac-auth/23.dialog-auth-reinvite-bye-uac \
-				 --exclude uac-auth/24.dialog-auth-reinvite-uac-reinvite-uas \
-				 --exclude uac-auth/25.dialog-auth-reinvite-uas-reinvite-uac \
-				 --exclude uac-auth/26.dialog-auth-reinvite-uac-reinvite-uas-bye-uas \
-				 --exclude uac-auth/27.dialog-auth-reinvite-uac-reinvite-uas-bye-uac \
 		}
 
 SETS=${SETS:-startup
