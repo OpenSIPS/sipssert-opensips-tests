f4e4145886bbd20a683d95e4f705df920c0e368e
Razvan Crainea <razvan@opensips.org>
2023-08-04 14:19:58 +0300

    do not exclude working uac-auth tests
    (cherry picked from commit 1313d03b6ecd1972f9d2facf69116c418fb40399)

--- run-all.sh
+++ run-all.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
 # exclude tests that we know they fail for sure
-PARAMS=${PARAMS:---exclude topology-hiding/02.th-no-dialog-username \
-				 --exclude uac-auth/17.dialog-auth-reinvite-uas \
-				 --exclude uac-auth/20.dialog-auth-invite-reinvite-bye-uas \
-				 --exclude uac-auth/22.dialog-auth-reinvite-uac \
-				 --exclude uac-auth/23.dialog-auth-reinvite-bye-uac \
-				 --exclude uac-auth/24.dialog-auth-reinvite-uac-reinvite-uas \
-				 --exclude uac-auth/25.dialog-auth-reinvite-uas-reinvite-uac \
-				 --exclude uac-auth/26.dialog-auth-reinvite-uac-reinvite-uas-bye-uas \
-				 --exclude uac-auth/27.dialog-auth-reinvite-uac-reinvite-uas-bye-uac \
-		}
+PARAMS=${PARAMS:---exclude topology-hiding/02.th-no-dialog-username}
 
 SETS=${SETS:-startup
 	registration \
