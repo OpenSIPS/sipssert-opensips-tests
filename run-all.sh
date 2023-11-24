7a8ef438b9cf81d2e0bd81529eae70df971bfe3d
Razvan Crainea <razvan@opensips.org>
2023-11-24 11:08:02 +0200

    run-all.sh: fix directory detection

--- run-all.sh
+++ run-all.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-DIR="$( dirname "${BASH_SOURCE[0]}")"
 yaml_list() {
+	test -n "${BASH_SOURCE[0]}" && DIR="$(dirname "${BASH_SOURCE[0]}")" || DIR=.
 	python3 -c "import yaml;print(' '.join(yaml.safe_load(open('$DIR/matrix.yml'))['$1']))"
 }
 
 # exclude tests that we know they fail for sure
 PARAMS=${PARAMS:-$(yaml_list params)}
 
-SETS=${SETS:-$(yaml_list scenario)}
+SETS=${SETS:-$(yaml_list test_set)}
 
 sipssert \
 	$PARAMS \
