e9c154d81765963ea75292eff9c11bcb535e4ae0
Razvan Crainea <razvan@opensips.org>
2023-11-24 10:58:00 +0200

    make run-all.sh use matrix.yml
    Avoid double provisioning by using the same config file
    
    (cherry picked from commit 291254b5dea894ca467e5fe75d42fd36e2c5290c)

--- run-all.sh
+++ run-all.sh
@@ -1,25 +1,14 @@
 #!/bin/bash
 
-# exclude tests that we know they fail for sure
-PARAMS=${PARAMS:---exclude b2b/09.refer-unattended-uac-notify-fail \
-				 --exclude b2b/10.refer-unattended-uas-notify-fail \
-				 --exclude b2b/15.refer-unattended-uac-prov-notify-fail \
-				 --exclude b2b/16.refer-unattended-uas-prov-notify-fail \
-				 --exclude topology-hiding/02.th-no-dialog-username \
-		}
+DIR="$( dirname "${BASH_SOURCE[0]}")"
+yaml_list() {
+	python3 -c "import yaml;print(' '.join(yaml.safe_load(open('$DIR/matrix.yml'))['$1']))"
+}
 
-SETS=${SETS:-startup
-	registration \
-	auth \
-	record-route \
-	dialog \
-	topology-hiding \
-	b2b \
-	uac-auth \
-	stir-shaken \
-	accounting \
-	presence \
-	permissions}
+# exclude tests that we know they fail for sure
+PARAMS=${PARAMS:-$(yaml_list params)}
+
+SETS=${SETS:-$(yaml_list scenario)}
 
 sipssert \
 	$PARAMS \
