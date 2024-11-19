4928797afcc2c4dd5f2d37ef514bf45c7c5440c8
Darius Stefan <darius.stefan@opensips.org>
2024-11-19 16:09:33 +0200

    Use new opensips-mi task

--- startup/02.gen-residential-cfg/check-ps.py
+++ startup/02.gen-residential-cfg/check-ps.py
@@ -1,9 +1,19 @@
-from opensipscli import cli
+from opensips.mi import OpenSIPSMI
 import sys
+import os
 
-handler = cli.OpenSIPSCLI()
+mi_type = os.getenv('MI_TYPE', 'http')
+mi_ip = os.getenv('MI_IP', '127.0.0.1')
+mi_port = os.getenv('MI_PORT', '8888')
 
-out = handler.mi("ps")
+if mi_type == 'http':
+    handler = OpenSIPSMI(conn='http', url='http://{}:{}/mi'.format(mi_ip, mi_port))
+elif mi_type == 'datagram':
+    handler = OpenSIPSMI(conn='datagram', datagram_ip=mi_ip, datagram_port=mi_port)
+else:
+    sys.exit(1)
+
+out = handler.execute("ps")
 
 if out:
 	sys.exit(0)
